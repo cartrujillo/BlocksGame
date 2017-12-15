01db501da7a26f002c330d4d334e6a0a399e730d
cartrujillo <carlatrujillo93@gmail.com>
2017-12-15 12:43:32 +0100

    Creación clases Tablero y Blocks

---
+++ BlocksGame/src/Block.java
@@ -0,0 +1,18 @@
+
+public class Block {
+	private int id;
+	private char orientation;
+	private int x;
+	private int y;
+	private int length;
+
+	public Block(int id, char orientation, int x, int y, int length) {
+		this.id = id;
+		this.orientation = orientation;
+		this.x = x;
+		this.y = y;
+		this.length = length;
+	}
+	
+	
+}
---
+++ BlocksGame/src/Tablero.java
@@ -0,0 +1,19 @@
+
+public class Tablero {
+	private int rows;
+	private int columns;
+	
+	public Tablero(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+}
